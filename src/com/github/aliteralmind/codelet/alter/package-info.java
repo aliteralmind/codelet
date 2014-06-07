a0a655703da8dbe1dc7d30d86401a022d507dd8d
aliteralmind <aliteralmind-github@yahoo.com>
2014-06-06 23:33:17 -0400

    Added alter package-info.java

---
+++ src/com/github/aliteralmind/codelet/alter/package-info.java
@@ -0,0 +1,4 @@
+/**
+   <P>Classes related to the {@linkplain com.github.aliteralmind.codelet.CustomizationInstructions alteration-portion} of a customizer function.</P>
+ **/
+package  com.github.aliteralmind.codelet.alter;
