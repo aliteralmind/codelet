512fb18d4654308e383e1dab1da9112f165a9b45
Jeff Epstein <aliteralmind-github@yahoo.com>
2014-12-11 12:57:44 -0500

    Simplified batch files

--- git_add_commit_push.bat
+++ git_add_commit_push.bat
@@ -5,19 +5,17 @@ REM    git_add_commit_push.bat
 
 REM call z_backup_..._data_folder_delete_existing_first.bat
 
-;set branch=%1
-;set commit_msg=%2
-set branch=master
-set commit_msg=%1
+set branch=%1
+set commit_msg=%2
 
-ECHO The commit message is the one and only command-line parameter.
+ECHO Two required command-line parameters: branch name, commit message.
 ECHO About to do
 ECHO 1.  git add --all :/
 ECHO 2.  git commit -m "%commit_msg%"
 ECHO 3.  git push -u origin %branch%
 REM PAUSE Press a key to proceed.
 
-git add --all
+git add --all :/
 REM PAUSE
 git commit -m %commit_msg%
 REM PAUSE
